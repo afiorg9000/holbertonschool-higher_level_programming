af9e7758ab62c432b087e672805ce544627c78f8
afiorg9000 <3533@holbertonschool.com>
2022-01-05 15:06:00 +0000

    push

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    m = []
+    for i in range(len(matrix)):
+        r = []
+        for  j in range(len(matrix[i])):
+            r.append(matrix[i][j]**2)
+        m.append(r)
+    return m
