c1f159b0c402cf1db11de44a01ab4f3f6bdd00d7
afiorg9000 <3533@holbertonschool.com>
2022-01-05 16:54:42 +0000

    pycode

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     m = []
     for i in range(len(matrix)):
         r = []
-        for  j in range(len(matrix[i])):
+        for j in range(len(matrix[i])):
             r.append(matrix[i][j]**2)
         m.append(r)
     return m
