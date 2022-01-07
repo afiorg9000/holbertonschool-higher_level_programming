98c068a0019d8a066eafe440670d381bb17ccdba
afiorg9000 <3533@holbertonschool.com>
2022-01-07 23:49:03 +0000

    push

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] += value
-    elif 2*key in a_dictionary:
-        a_dictionary[2*key] += [value]
-    else:
-        a_dictionary.update({2*key:[value]})
+    a_dictionary[key] = value
+    return (a_dictionary)
