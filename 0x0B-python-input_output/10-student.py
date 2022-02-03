1411bc524b3ec69b63664407f191f5b1f6ed52e8
afiorg9000 <3533@holbertonschool.com>
2022-02-03 03:44:40 +0000

    push

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-import json
+"""Contains the clas "Student"""
+
 
 class Student:
+    """Representation of a student"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        if type(attrs) is list:
-            x = {}
-            for i in range(len(attrs)):
-                if attrs[i] in self.__dict__:
-                    x[attrs[i] = self.__dict__[attrs[i]]
-
-            return x
-
-        else:
+    def to_json(self, attrs=None):
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-
-
-
