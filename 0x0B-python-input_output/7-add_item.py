93c91d398104bbda7ae18ab649d9a27d06d65f03
afiorg9000 <3533@holbertonschool.com>
2022-02-03 03:24:27 +0000

    push

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-import json
-def add_item():
+"""adds argv to file"""
+import sys
 
-    list = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
 
-    for i in range (1, len(sys.argv)):
-        list.append(sys.argv[i])
-
-        with open ('
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    ave_to_json_file(items, "add_item.json")
