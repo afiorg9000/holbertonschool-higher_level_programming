17a8e7051f89f3cbf34976a7ea7c3fae89bee87d
afiorg9000 <3533@holbertonschool.com>
2022-02-03 23:30:10 +0000

    push

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """Invert int operators"""
+
+    def __eq__(self, value):
+        return self.real != value
+
+    def __ne__(self, value):
+        return self.real == value
