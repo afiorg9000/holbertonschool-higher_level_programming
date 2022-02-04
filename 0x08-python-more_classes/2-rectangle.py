d674725a0ed4dc71885f183d7387e4b624df9e45
afiorg9000 <3533@holbertonschool.com>
2022-02-04 02:18:48 +0000

    push

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -15,13 +15,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-
-        if type(value) is not int:
+        self.__width = value
+        if isinstance(value, int) is not True:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
 
     @property
     def height(self):
@@ -29,13 +27,11 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-
+        self.__height = value
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
 
     def area(self):
         return (self.width * self.height)
