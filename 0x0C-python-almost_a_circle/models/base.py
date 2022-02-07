075f5113f422ccc91fcc85699d09c5f390fa9eed
afiorg9000 <3533@holbertonschool.com>
2022-02-07 16:45:42 +0000

    push

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,9 +3,11 @@
 
 
 class Base:
+    """Base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize method"""
         if id is not None:
             self.id = id
         else:
