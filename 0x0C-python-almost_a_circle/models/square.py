1572a47f7f8a04b107ade96aca44cdcfdae44673
afiorg9000 <3533@holbertonschool.com>
2022-02-07 00:18:50 +0000

    push

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,17 +2,12 @@
 """Defines a square class."""
 from models.rectangle import Rectangle
 
-"""
-Task 10.
-"""
+
 class Square(Rectangle):
     """Represent a square."""
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square"""
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.height)
 
     @property
     def size(self):
@@ -22,3 +17,20 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        args_list = ["id", "width", "x", "y"]
+        if args and args[0] is not None:
+            if len(args) > len(args_list):
+                length = len(args_list)
+            else:
+                length = len(args)
+            for tmp in range(length):
+                setattr(self, args_list[tmp], args[tmp])
+        elif kwargs is not None:
+            for key in kwargs:
+                if hasattr(self, key) is True:
+                    setattr(self, key, kwargs[key])
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.height)
