2af4714a1bbeff400759ef907d975d8fc1742f55
afiorg9000 <3533@holbertonschool.com>
2022-02-08 01:14:07 +0000

    push

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,3 +36,11 @@ class Base:
             else:
                 list_dictionaries = [o.to_dictionary() for o in list_objs]
                 jsonfile.write(Base.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns a list of dictionaries from a JSON string"""
+        if json_string is None or json_string == "[]":
+            return []
+        else:
+            return json.loads(json_string)
