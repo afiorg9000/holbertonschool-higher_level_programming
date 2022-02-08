640d5fdfe8556800f2b73ca4905fab3b06773b84
afiorg9000 <3533@holbertonschool.com>
2022-02-08 19:42:06 +0000

    push

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,27 +6,28 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
+    """class for base class"""
 
     @classmethod
     def setUpClass(cls):
+        """test init class"""
         cls.base1 = Base()
         cls.base2 = Base()
         cls.base3 = Base()
         cls.base4 = Base(20)
 
     def test_id(self):
+        """method that tests id"""
         self.assertEqual(self.base1.id, 1)
         self.assertEqual(self.base2.id, 2)
         self.assertEqual(self.base3.id, 3)
 
     def test_id_parameter(self):
+        """method that tests id parameter"""
         self.assertEqual(self.base4.id, 20)
 
     def test_increment(self):
+        """method that tests increment"""
         Base.__nb_objects = 0
 
         self.assertEqual(self.base1.id, 1)
-
-
-if __name__ == "__main__":
-    unittest.main()
