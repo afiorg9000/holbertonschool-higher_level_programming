297d6a16ea2e2499ff6744bcc484e1d040805af7
afiorg9000 <3533@holbertonschool.com>
2022-02-08 00:48:05 +0000

    push

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Return the JSON serialization of list_dictionaries"""
 
@@ -25,9 +26,13 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
+    @classmethod
     def save_to_file(cls, list_objs):
         """"writes the JSON string representation of list_objs to a file"""
-        if list_objs is None:
-            jsonfile.write("[]")
-        else:
-            jsonfile.write(Base.to_json_string)
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dictionaries = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dictionaries))
