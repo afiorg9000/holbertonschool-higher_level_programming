6a8bfa459985e63d7cbf8b279820de3b85986903
afiorg9000 <3533@holbertonschool.com>
2022-04-04 18:33:28 -0400

    i love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 const x = process.argv[2];
 if (x) {
-    for (let tmp = 0; tmp < x; tmp++) {
-      console.log('C is fun');
-    }
-} else { console.log('Missing number of occurrences')};
+  for (let tmp = 0; tmp < x; tmp++) {
+    console.log('C is fun');
+  }
+} else { console.log('Missing number of occurrences'); }
