fd4a301fab18ed3e10006404ea18296811a1e2a6
afiorg9000 <3533@holbertonschool.com>
2022-04-04 19:18:09 -0400

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,6 +6,6 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let idx = 0; idx < size; idx++) {
-    console.log('X '.repeat(size));
+    console.log('X'.repeat(size));
   }
 }
