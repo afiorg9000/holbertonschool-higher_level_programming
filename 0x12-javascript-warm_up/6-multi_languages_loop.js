b01ff6399c0c21ffa39d7eb2b61d7a01309937cf
afiorg9000 <3533@holbertonschool.com>
2022-04-04 18:14:53 -0400

    languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let lan = 0; lan < languages.length; lan++) {
+  console.log(languages[lan]);
+}
