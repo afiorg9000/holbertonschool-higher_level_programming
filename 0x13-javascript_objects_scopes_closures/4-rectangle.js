64af9de17fa31f79f64837289b32b5ce10a15ea7
afiorg9000 <3533@holbertonschool.com>
2022-04-05 15:24:42 -0400

    rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let idx = 0; idx < this.height; idx++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
