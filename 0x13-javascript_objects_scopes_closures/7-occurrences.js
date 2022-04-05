ade1fcf0e1d18c000c58c91c6b8f71dd01ade738
afiorg9000 <3533@holbertonschool.com>
2022-04-05 15:59:35 -0400

    push

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+
+  for (let idx = 0; idx < list.length; idx++) {
+    if (list[idx] === searchElement) {
+      count += 1;
+    }
+  }
+
+  return count;
+};
