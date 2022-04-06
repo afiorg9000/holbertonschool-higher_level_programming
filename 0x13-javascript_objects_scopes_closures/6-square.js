c1076293e7ec9414932b137a00e4368483c41b00
afiorg9000 <3533@holbertonschool.com>
2022-04-06 11:50:22 -0400

    export fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
-const Square2 = require('./5-square');
-module.exports = class Square extends Square2 {
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  
   charPrint (c) {
     if (c === undefined) {
       this.print();
