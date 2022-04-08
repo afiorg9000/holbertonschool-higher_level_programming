1db305a25783cb3fd70e7af0cbd817d41bc3119c
afiorg9000 <3533@holbertonschool.com>
2022-04-08 14:03:35 -0400

    push

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     # prepare a cursor object using cursor() method
     cur = db.cursor()
     # execute SQL query using execute() method.
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}'".format(sys.argv[4]))
     
     # Fetch a single row using fetchone() method.
     rows = cur.fetchall()
     for row in rows:
-        if row[1][0] == 'N':
+        if row[1] == sys.argv[4]:
             print(row)
  
     cur.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
 
+from distutils.util import execute
 import sys
 import pymysql as MySQLdb
 
+
 if __name__ == "__main__":
     # opens database connection
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd="test", db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd="test", db=sys.argv[3], charset="utf8")
     # prepare a cursor object using cursor() method
     cur = db.cursor()
+    state = sys.argv[4]
     # execute SQL query using execute() method.
-    cur.execute("SELECT * FROM states")
-    
+    query = ("""SELECT * FROM states WHERE name = %s""")
+    parameters = (state,)
+    cur.execute(query, parameters)
     # Fetch a single row using fetchone() method.
     rows = cur.fetchall()
+
     for row in rows:
-        if row[1][0] == 'N':
+        if row[1] == state:
             print(row)
  
     cur.close()
