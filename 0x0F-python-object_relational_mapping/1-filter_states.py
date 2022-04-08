d980eadf87be243116e18801d6708dc067a4a47d
afiorg9000 <3533@holbertonschool.com>
2022-04-08 11:10:46 -0400

    filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+import pymysql as MySQLdb
+
+if __name__ == "__main__":
+    # opens database connection
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd="test", db=sys.argv[3])
+    # prepare a cursor object using cursor() method
+    cur = db.cursor()
+    # execute SQL query using execute() method.
+    cur.execute("SELECT * FROM states")
+    
+    # Fetch a single row using fetchone() method.
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+
+    cur.close()
+    db.close()
