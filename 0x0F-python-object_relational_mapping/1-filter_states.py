92b1efcd6904fd8d69db4c8af1db0e9c088f135e
afiorg9000 <3533@holbertonschool.com>
2022-04-09 11:43:25 -0400

    pycodestyle task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-lists all states from the database hbtn_0e_0_usa
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 """
 
 import sys
