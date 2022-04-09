5edd85ca5129367404496464d94ab50c9b504fa8
afiorg9000 <3533@holbertonschool.com>
2022-04-09 11:35:10 -0400

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
 
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    # opens database connection
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    # prepare a cursor object using cursor() method
+    """opens database connection"""
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    """prepare a cursor object using cursor() method"""
     cur = db.cursor()
-    # execute SQL query using execute() method.
+    """execute SQL query using execute() method."""
     cur.execute("SELECT * FROM states")
-    
-    # Fetch a single row using fetchone() method.
+    """Fetch a single row using fetchone() method."""
     rows = cur.fetchall()
     for row in rows:
         print(row)
