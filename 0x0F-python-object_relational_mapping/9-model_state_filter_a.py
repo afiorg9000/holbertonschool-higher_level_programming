3e9b4f4d8b522c02c0043e58289e5beeb01c1f8c
afiorg9000 <3533@holbertonschool.com>
2022-04-09 17:02:00 +0000

    push

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     session = Session(engine)
 
-    for state in session.query(State)
+    for state in session.query(State)\
     .filter(State.name.like('%a%')).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
