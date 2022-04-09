174bcff05d5d1120d9a2797ac09c7c6cbfabd334
afiorg9000 <3533@holbertonschool.com>
2022-04-09 12:20:22 -0400

    styling task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
 
+"""
+takes in the name of a state as an argument and lists all cities of that state,
+using the database hbtn_0e_4_usa
+"""
 from distutils.util import execute
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    # opens database connection
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    # prepare a cursor object using cursor() method
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    # execute SQL query using execute() method.
-    cur.execute("""SELECT cities.name
-    FROM cities
-    JOIN states
-    ON state_id=states.id
-    WHERE states.name LIKE BINARY %s
-    ORDER BY cities.id
-    """, (sys.argv[4],))
-
-    # Fetch a single row using fetchone() method.
+    cur.execute("SELECT cities.name FROM cities JOIN states\
+    ON state_id=states.id WHERE states.name LIKE BINARY %s\
+    ORDER BY cities.id", (sys.argv[4],))
     rows = cur.fetchall()
-
     print(", ".join([row[0] for row in rows]))
- 
     cur.close()
     db.close()
