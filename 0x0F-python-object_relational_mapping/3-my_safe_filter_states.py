ad5fc3381f21940de033bd8412c08f86d650a3fb
afiorg9000 <3533@holbertonschool.com>
2022-04-09 12:07:56 -0400

    pystyle task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 
+"""
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
 from distutils.util import execute
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    # opens database connection
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    # prepare a cursor object using cursor() method
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = db.cursor()
     state = sys.argv[4]
-    # execute SQL query using execute() method.
     query = ("""SELECT * FROM states WHERE name = %s""")
     parameters = (state,)
     cur.execute(query, parameters)
-    # Fetch a single row using fetchone() method.
     rows = cur.fetchall()
-
     for row in rows:
         if row[1] == state:
             print(row)
- 
     cur.close()
     db.close()
