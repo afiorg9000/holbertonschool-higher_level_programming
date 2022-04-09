797311173d914e4e5613a9ce009f4b0f3b08ce3e
afiorg9000 <3533@holbertonschool.com>
2022-04-09 12:27:44 -0400

    styling state class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
+"""
+Start link class to table in database
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
 
+
 class State(Base):
+    """Represents a state for a MySQL database."""
+
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
