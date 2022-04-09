6eecb5d5d1d02c81f9842700d2cda07615ce24af
afiorg9000 <3533@holbertonschool.com>
2022-04-09 12:05:03 -0400

    pycodestyle task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
+"""
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # opens database connection
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    # prepare a cursor object using cursor() method
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    # execute SQL query using execute() method.
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}'".format(sys.argv[4]))
-    
-    # Fetch a single row using fetchone() method.
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}'"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         if row[1] == sys.argv[4]:
             print(row)
- 
     cur.close()
     db.close()
