f4d2900c89e07fda9e562528cc8476adfd030b45
afiorg9000 <3533@holbertonschool.com>
2022-04-09 11:38:33 -0400

    pycodestyle task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # opens database connection
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    # prepare a cursor object using cursor() method
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    # execute SQL query using execute() method.
     cur.execute("SELECT * FROM states")
-    
-    # Fetch a single row using fetchone() method.
+
     rows = cur.fetchall()
     for row in rows:
         if row[1][0] == 'N':
             print(row)
- 
     cur.close()
     db.close()
