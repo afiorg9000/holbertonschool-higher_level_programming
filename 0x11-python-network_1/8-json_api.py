201f121c9ece1390702dc9cd5aa274e770167ace
afiorg9000 <3533@holbertonschool.com>
2022-04-26 18:38:56 +0000

    checkers

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,10 +6,9 @@ import sys
 
 if __name__ == "__main__":
     try:
-        data = {"q": ""}
-
-    except:
         data = {"q": sys.argv[1]}
+    except:
+        data = {"q": ""}
 
     try:
         url = requests.post("http://0.0.0.0:5000/search_user", data)
