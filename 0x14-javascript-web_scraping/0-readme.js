04bccdf75e40d5effb91a46560758fb1ad854a95
afiorg9000 <3533@holbertonschool.com>
2022-05-17 18:10:21 +0000

    im in love with semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 const FileName = process.argv[2];
 
-fs.readFile(FileName, "utf8", function (err, data) {
+fs.readFile(FileName, 'utf8', function (err, data) {
   if (err) {
     console.error(err);
     return;
