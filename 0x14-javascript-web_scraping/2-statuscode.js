133d06476dbbedce2e4b699eff025b1097b13990
afiorg9000 <3533@holbertonschool.com>
2022-05-17 21:29:21 +0000

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const axios = require('axios');
 const url = process.argv[2];
-let statusCode;
 
 axios.get(url).then((response) => {
-	console.log(`code: ${response.status}`);
+  console.log(`code: ${response.status}`);
 }).catch((error) => {
-	console.log('Error', error);
+  console.log('Error', error);
 });
